c51ec70a8e71f2e8e7a0d0bb4f0712b379af0505
Arun Babu Neelicattu <abn@redhat.com>
2013-09-02 23:03:43 +1000

    Add helper method to plugin pkg to handle configs

---
+++ src/victims_web/plugin/__init__.py
@@ -0,0 +1,30 @@
+# This file is part of victims-web.
+#
+# Copyright (C) 2013 The Victims Project
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from victims_web.models import Plugin
+
+
+def get_config(plugin):
+    """
+    Helper to get a plugin's configuration
+    """
+    config = Plugin.objects(plugin=plugin).first()
+    if config is None:
+        config = Plugin()
+        config.plugin = plugin
+        config.save()
+    return config
