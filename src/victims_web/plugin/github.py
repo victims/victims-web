6e53ec44577be6e347a24b9b7cff13b49240976a
Arun Babu Neelicattu <abn@redhat.com>
2013-09-02 23:01:48 +1000

    Adding Repository class to github plugin to allow for leveraging repositories via native git

--- src/victims_web/plugin/github.py
+++ src/victims_web/plugin/github.py
@@ -18,12 +18,22 @@
 GitHub plugin
 """
 
+from os import walk, sep
+from os.path import join, isdir
+from shutil import rmtree
 from urlparse import urljoin
+from re import search
 from requests import get
+from subprocess import check_output
+
+from victims_web.config import DOWNLOAD_FOLDER
+
+
+BASE_URI = 'https://github.com/'
+API_URI = 'https://api.github.com/'
 
 
 class GitHub():
-    API_URI = 'https://api.github.com/'
 
     def __init__(self, user, repo):
         self.user = user
@@ -41,3 +51,82 @@ class GitHub():
     #/repos/:owner/:repo/commits/:sha
     def get_commit(self, sha, **kwargs):
         return self._rest('repos', 'commits/%s' % (sha), **kwargs)
+
+
+class Repository():
+
+    def __init__(self, user, repo, basedir=DOWNLOAD_FOLDER):
+        self.user = user
+        self.repo = repo
+        self.basedir = basedir
+        self.repodir = join(basedir, repo)
+        self.repourl = urljoin(BASE_URI, '%s/%s' % (user, repo))
+
+    def is_cloned(self):
+        return isdir(self.repodir)
+
+    def execute(self, cmd, *args):
+        gitcmd = ['git']
+
+        if cmd != 'clone':
+            if not self.is_cloned():
+                return None
+            gitcmd.append('--git-dir=%s/.git' % (self.repodir))
+        else:
+            if self.is_cloned():
+                return None
+
+        gitcmd.append(cmd)
+
+        for arg in args:
+            gitcmd.append(arg)
+        output = check_output(gitcmd)
+
+        return output
+
+    def clone(self, force=False, *args):
+        if force:
+            rmtree(self.repodir)
+        self.execute('clone', self.repourl + '.git', self.repodir, *args)
+
+    def pull(self, *args):
+        return self.execute('pull', *args)
+
+    def log(self, *args):
+        return self.execute('log', *args)
+
+    def diff(self, *args):
+        return self.execute('diff', *args)
+
+    def head(self):
+        return self.log('-n 1', '--pretty=%H').strip()
+
+    def filter_files(self, files, path, pattern=None):
+        filtered = []
+        for f in files:
+            f = f.strip()
+            if f.startswith(path):
+                if not pattern or search(pattern, f):
+                    filtered.append(f)
+
+        return filtered
+
+    def absolute_filepath(self, relative):
+        return join(self.repodir, relative)
+
+    def files_changed(self, start, end='HEAD', path='', pattern=None):
+        self.pull()
+        files = self.diff('--name-only', start, end).split()
+        return self.filter_files(files, path, pattern)
+
+    def files(self, path='', pattern=None):
+        oldpath = path
+        path = join(self.repodir, path)
+        files = []
+        if isdir(path):
+            for root, dirs, fs in walk(path):
+                for f in fs:
+                    if not pattern or search(pattern, f):
+                        files.append(join(
+                            root.replace(path, oldpath).strip(sep), f))
+        return files
