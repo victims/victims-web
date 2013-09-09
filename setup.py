073db7b230987089782d80d464424c4385e7c2e1
Arun Babu Neelicattu <abn@redhat.com>
2013-09-09 17:32:47 +1000

    Update version to 2.0.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup
 
 setup(
     name='victims_web',
-    version='1.9.9',
+    version='2.0.0',
     description='The victi.ms language package to CVE service.',
     author='Steve Milner',
     url='http://www.victi.ms',
